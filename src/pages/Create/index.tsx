470bd1437dee9d049d0ec69077d754c2023da439
Aryan sahu <rynsahu@gmail.com>
2025-03-02 23:01:50 +0530

    added create link navigation

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,11 +1,17 @@
 import MainPage from "@/components/common/MainPage";
+import { useNavigate } from "react-router-dom";
 
 const CreatePage = () => {
+  const navigate = useNavigate();
+
   return (
     <MainPage 
       heading="Create a link"
       subHeading="You can create 1000 more links this month."
-      actionBtn={{ label: 'Home' }}
+      actionBtn={{ 
+        label: 'Home',
+        handleActionBtnClick: () => navigate('/home')
+      }}
     >
       <p>Body</p>
     </MainPage>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import MainPage from "@/components/common/MainPage";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <MainPage 
       heading="Home"
       subHeading="You can create 1000 more links this month."
-      actionBtn={{ label: 'Create Link' }}
+      actionBtn={{ 
+        label: 'Create Link',
+        handleActionBtnClick: () => navigate('/links/create')
+      }}
     >
       <p>Body</p>
     </MainPage>
